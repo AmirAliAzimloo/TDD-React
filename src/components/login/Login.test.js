e850203832a27b2671317b5f003f2de7312cb6a1
Amir Ali <amiraliazimloo123@gmail.com>
2023-11-10 00:04:34 +0330

    testing for changing inputs completed

--- src/components/login/Login.test.js
+++ src/components/login/Login.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Login from "./Login"
 
 test("username input should be rendered",()=>{
@@ -49,4 +49,24 @@ test("error should not be visible",()=>{
 
     const errorEl = screen.getByTestId("error")
     expect(errorEl).not.toBeVisible() 
+})
+
+
+
+test("username input should change",()=>{
+    render(<Login/>)
+
+    const usernameEl = screen.getByPlaceholderText(/username/i)
+    const testValue = "value"
+    fireEvent.change(usernameEl,{target : { value : testValue }})
+    expect(usernameEl.value).toBe(testValue)
+})
+
+test("password input should change",()=>{
+    render(<Login/>)
+
+    const passwordEl = screen.getByPlaceholderText(/password/i)
+    const testValue = "test"
+    fireEvent.change(passwordEl,{ target: { value : testValue } })
+    expect(passwordEl.value).toBe(testValue)
 })
