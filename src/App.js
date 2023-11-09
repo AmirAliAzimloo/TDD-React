51036c251759fb1337b8b58d30809d246ac4e17f
Amir Ali <amiraliazimloo123@gmail.com>
2023-11-09 22:20:43 +0330

    start testing login component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   const a = 3;
+//   const b = 3;
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           learn react
+//         </a>
+//         <ul>
+//           <li>li1</li>
+//           <li>li2</li>
+//           <li>li3</li>
+//         </ul>
+//         <h1 data-testid="mytestid" >hello</h1>
+//         <span title='sum' >{a+b}</span>
+//       </header>
+//     </div>
+    
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
+import Login from './components/login/Login';
 
 function App() {
-  const a = 3;
-  const b = 3;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          learn react
-        </a>
-        <ul>
-          <li>li1</li>
-          <li>li2</li>
-          <li>li3</li>
-        </ul>
-        <h1 data-testid="mytestid" >hello</h1>
-        <span title='sum' >{a+b}</span>
-      </header>
+      <Login/>
     </div>
   );
 }
